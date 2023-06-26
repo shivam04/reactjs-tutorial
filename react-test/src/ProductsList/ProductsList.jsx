bab818662a5a520793552c67ba45d496904d8f1a
Shivam Sinha <shvmsnha@amazon.com>
2023-06-26 23:12:39 +0530

    state class 1

--- react-test/src/ProductsList/ProductsList.jsx
+++ react-test/src/ProductsList/ProductsList.jsx
@@ -1,5 +1,6 @@
 import ProductCard from '../ProductCard'
 import "./ProductsList.css";
+import { useState } from 'react';
 
 const products = [
     {
@@ -28,14 +29,45 @@ const products = [
     }
   ];
 
-function ProductsList() {
-    return ( 
-        <div className="products">
-            {products.map(function (product) {
-                return <ProductCard title={product.title} price={product.price} />;
-            })}
-        </div>
-     );
+function getProductsApi(callback) {
+  setTimeout(function () {
+    callback(products);
+  }, 1000);
 }
+
+function ProductsList() {
+  let [isLoading, setLoadingState] = useState(true);
+  let [allProducts, setAllProducts] = useState([]);
+  getProductsApi(function (res) {
+    setAllProducts(res);
+    setLoadingState(false);
+  });
+  if (isLoading) {
+    return <div>Loading....</div>
+  } else {
+    return ( 
+      <div className="products">
+          {allProducts.map(function (product) {
+              return <ProductCard title={product.title} price={product.price} />;
+          })}
+      </div>
+    );
+  }
+}
+
+// what is state variable?
+// state is a variable whose value when set rerender the whole component.
+// How do we create state variable?
+// React hooks
+// React hooks are function in js
+// hooks are helper function.
+// useState hook
+// useEffect
+// useName
+// useCallback
+
+// state variable going to persist the state even with re-render.
+
+// only child re-render
 
 export default ProductsList;
