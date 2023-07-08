845f296b733e1550fb5bd780f44bed7c8775fb7f
shivam04 <sinhashivam04@gmail.com>
2023-07-08 14:54:53 +0530

    useEffect hook

--- react-test/src/ProductsList/ProductsList.jsx
+++ react-test/src/ProductsList/ProductsList.jsx
@@ -4,26 +4,32 @@ import { useEffect, useState } from 'react';
 
 const products = [
     {
+      id: 1,
       title: "Apple iPhone 14",
       price: "Rs. 1,00,000"
     },
     {
+      id: 2,
       title: "Apple iPhone 13",
       price: "Rs. 70,000"
     },
     {
+      id: 3,
       title: "Google Pixel 7",
       price: "Rs. 50,000"
     },
     {
+      id: 4,
       title: "Nokia 1100",
       price: "Rs. 2,000"
     },
     {
+      id: 5,
       title: "Samsung Galaxy S10",
       price: "Rs. 1,00,000"
     },
     {
+      id: 6,
       title: "Sony Xperia S10",
       price: "Rs. 1,00,000"
     }
@@ -40,6 +46,7 @@ function ProductsList() {
   let [allProducts, setAllProducts] = useState([]);
   // useEffect would be called once on mounting
   // next everytime on state change of dependency array variables
+  // add [] in useEffect render only once during mounting
   useEffect(() => {
     getProductsApi(function (res) {
       console.log("api started1");
@@ -47,14 +54,26 @@ function ProductsList() {
       setLoadingState(false);
       console.log("api ended");
     });
-  });
+  }, [allProducts, isLoading]);
+  // function useEffect(callback, dependency array) {var counter;if(dep===[]&& counter<1){counter++; callback()} if(count)if(dep[0]!==prevdep[0]){callback()}}
+  // setIsLoading(true);
   if (isLoading) {
-    return <div>Loading....</div>
+    return (
+      <img 
+        alt="loading"
+        src="https://miro.medium.com/v2/resize:fit:720/1*e_Loq49BI4WmN7o9ItTADg.gif"
+      />)
   } else {
     return ( 
       <div className="products">
           {allProducts.map(function (product) {
-              return <ProductCard title={product.title} price={product.price} />;
+              return (
+                <ProductCard 
+                  title={product.title} 
+                  price={product.price} 
+                  key={product.id}
+                />
+              );
           })}
       </div>
     );
